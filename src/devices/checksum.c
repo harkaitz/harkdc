de467f13f522883654194375ac6b4836d61c0f67
Harkaitz Agirre Ezama <harkaitzv@gmail.com>
2018-07-16 05:07:28 +0200

    fix

--- src/devices/checksum.c
+++ src/devices/checksum.c
@@ -1,7 +1,7 @@
 #include "checksum.h"
 u_int8_t checksum8(u_int8_t *array,u_int8_t len) {
      u_int32_t chk = 0;
-     for(int i=0;i<len;i++) {
+     for(u_int8_t i=0;i<len;i++) {
 	  chk = (chk + array[i]); //  % 256
      }
      return chk % 256;
